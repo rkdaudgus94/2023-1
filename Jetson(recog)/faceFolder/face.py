cfd521728e089d9238e07fdebd7695099ce7311f
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2023-02-09 20:00:48 +0900

    Update face.py

--- Jetson(recog)/faceFolder/face.py
+++ Jetson(recog)/faceFolder/face.py
@@ -17,23 +17,31 @@ def face_confidence(face_distance, face_match_threshold=0.6): # face_distance ê°
         return str(round(value, 2)) + '%'
 
 def gstreamer_pipeline(
-        sensor_id=0,
-        capture_width=1920,
-        capture_height=1080,
-        display_width=960,
-        display_height=540,
-        framerate=30,
-        flip_method=0,
-        ) :
-        return (
-        sensor_id,
-        capture_width,
-        capture_height,
-        display_width,
-        display_height,
-        framerate,
-        flip_method,
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
         )
+    )
 
 image_path = r'/home/hyun/face_img/*.png'
 
